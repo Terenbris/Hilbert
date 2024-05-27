427b20630473c8c57ec07663c1fe2b3b25ab5bc4
Terenbris <51034503+Terenbris@users.noreply.github.com>
2024-05-26 22:02:46 -0500

    add CreateQuadrant Method

--- Hilbert.py
+++ Hilbert.py
@@ -1,5 +1,14 @@
 def CreateArray(size):
     board = [[[0,0] for i in range(size)] for j in range(size)]
+    CreateQuadrant(size)
     print(board)
+
+def CreateQuadrant(height):
+    wh = height/2
+
+    if wh < 2:
+        return
+    else:
+        CreateQuadrant(wh)
 
 CreateArray(2)
