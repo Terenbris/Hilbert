99e09a5291f91cb35f793c7156de7d3867d0ae6f
Terenbris <51034503+Terenbris@users.noreply.github.com>
2024-05-26 23:07:14 -0500

    add direction logic

--- Hilbert.py
+++ Hilbert.py
@@ -3,8 +3,18 @@ def CreateArray(size):
     CreateQuadrant(size)
     print(board)
 
-def CreateQuadrant(height):
-    wh = height/2
+def CreateQuadrant(height, dir):
+    wh = height/2 
+    for i in range(2):
+        for j in range(2):
+            if dir == 0:
+                wd = ((i*3)-(i*j*3))+(i*j)
+            elif dir == 1:
+                wd = j+((i*2)-(i*j*2))
+            elif dir == 2:
+                wd = 3-(j*2)-i+(i*j*2)
+            elif dir == 3:
+                wd = 3-(j*3)+(i*j*2)
 
     if wh < 2:
         return
